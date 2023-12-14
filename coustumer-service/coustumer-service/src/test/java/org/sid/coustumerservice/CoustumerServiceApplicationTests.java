3e476d39cc6fb5e31cf3b03826ffe1f584a845d0
YassineElabdaoui <eyassine381@gmail.com>
2023-12-14 15:11:28 +0100

    Commit message here

---
+++ coustumer-service/coustumer-service/src/test/java/org/sid/coustumerservice/CoustumerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sid.coustumerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoustumerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ order-service/src/main/java/org/sid/orderservice/ennums/OrderStatus.java
@@ -0,0 +1,5 @@
+package org.sid.orderservice.ennums;
+
+public enum OrderStatus {
+    CREATED, PENDING, DELIVERED, CANCELED
+}
